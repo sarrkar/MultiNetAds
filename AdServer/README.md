794524e41ecaa25309078dee01f691cac5498911
Attar81 <amirhoseinatari@gmail.com>
2024-07-22 16:06:42 +0330

    add AdServer mock

---
+++ AdServer/README.md
@@ -0,0 +1,49 @@
+endpoints:
++ `GET`: `adserver.local/api/ad`
+    * `res`: json:
+    ```json
+    {
+        "ad": {
+            "Title": "title1",
+            "ImageUrl": "image.storage/media/image12.jpg",
+            "ImpressionUrl": "eventserver.local/impression/12/EVoSxr4loX",
+            "ClickUrl": "eventserver.local/click/12/c6l1kfnery"
+        }
+    }
+    ``` 
+
+
+expected endpoints:
++ `POST`: `panel.local/api/ads` 
+    * `res`: json:
+    ```json
+    {
+        "ads": [
+            {
+                "ID": 12,
+                "Title": "title1",
+                "ImageUrl": "image.storage/media/image12.jpg",
+                "BID": 100
+            },
+            {
+                "ID": 14,
+                "Title": "title2",
+                "ImageUrl": "image.storage/media/image14.jpg",
+                "BID": 150
+            }
+        ]
+    }
+    ```
++ `GET` `eventserver.local/impression/:id/:OTLkey`
++ `GET` `eventserver.local/click/:id/:OTLkey`
+
+in go:
+
+```go
+c.IndentedJSON(http.StatusOK, gin.H{
+    "ads": []Ad{
+        {12, "title1", "image.storage/media/image12.jpg", 100},
+        {14, "title2", "image.storage/media/image14.jpg", 150},
+    },
+})
+```
---
+++ AdServer/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Ads []Ad
+}
+
+type Ad struct {
+	ID       uint
+	Title    string
+	ImageUrl string
+	BID      uint
+}
+
+type AdResponse struct {
+	Title         string
+	ImageUrl      string
+	ImpressionUrl string
+	ClickUrl      string
+}
+
+func randStr(length int) string {
+	b := make([]byte, length)
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func main() {
+	// resp, err := http.Get("https://gobyexample.com")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer resp.Body.Close()
+	// b, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(string(b))
+
+	jsonTest := `
+{
+    "ads": [
+        {
+            "ID": 12,
+            "Title": "title1",
+            "ImageUrl": "image.storage/media/image12.jpg",
+            "BID": 100
+        },
+        {
+            "ID": 14,
+            "Title": "title2",
+            "ImageUrl": "image.storage/media/image14.jpg",
+            "BID": 150
+        }
+    ]
+}`
+	b := []byte(jsonTest)
+
+	var res Response
+	json.Unmarshal(b, &res)
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		// c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("service is up!"))
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"ad": AdResponse{
+				Title:         res.Ads[0].Title,
+				ImageUrl:      res.Ads[0].ImageUrl,
+				ImpressionUrl: fmt.Sprintf("eventserver.local/impression/%d/%s", res.Ads[0].ID, randStr(10)),
+				ClickUrl:      fmt.Sprintf("eventserver.local/click/%d/%s", res.Ads[0].ID, randStr(10)),
+			},
+		})
+	})
+	router.Run(":8080")
+}
