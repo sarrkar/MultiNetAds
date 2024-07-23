b88ce39b5b59c50a0bdb4db82cb6058e4c72afe0
sarrkar <sarraah.karimi@gmail.com>
2024-07-23 17:05:56 +0330

    update templates

--- Panel/main.go
+++ Panel/main.go
@@ -1,19 +1,12 @@
 package main
 
-import (
-	"log"
-
-	"github.com/sarrkar/Chan-ta-net/Panel/api"
-	"github.com/sarrkar/Chan-ta-net/Panel/database"
-)
-
 func main() {
 
-	err := database.InitDb()
-	defer database.CloseDb()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	//err := database.InitDb()
+	//defer database.CloseDb()
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
 
-	api.InitServer()
+	//api.InitServer()
 }
