8d73dd1d73e509101929583f5889792aad47b23a
Attar81 <amirhoseinatari@gmail.com>
2024-07-24 14:17:54 +0330

    fix panel models

--- Panel/models/ad.go
+++ Panel/models/ad.go
@@ -1,13 +1,11 @@
 package models
 
-import "gorm.io/gorm"
-
 type Ad struct {
-	gorm.Model
+	ID           int    `gorm:"column:id; primary_key; not null" json:"id"`
 	Title        string `json:"title"`
 	ImageUrl     string `json:"image_url"`
 	RedirectUrl  string `json:"redirect_url"`
-	BID          uint   `json:"bid"`
+	BID          uint   `gorm:";column:bid" json:"bid"`
 	Active       bool   `json:"active"`
 	Impression   uint   `json:"impression"`
 	Click        uint   `json:"click"`
