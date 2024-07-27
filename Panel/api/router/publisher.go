5a040647ee85f8d810be4f9de45e92d0a9e6b6ab
sarrkar <sarraah.karimi@gmail.com>
2024-07-27 13:54:38 +0330

    publisher controller added, update publisher panel

--- Panel/api/router/publisher.go
+++ Panel/api/router/publisher.go
@@ -2,15 +2,16 @@ package router
 
 import (
 	"net/http"
-//	"strconv"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-//	"github.com/sarrkar/chan-ta-net/panel/api/controller"
-//	"github.com/sarrkar/chan-ta-net/panel/models"
+	"github.com/sarrkar/chan-ta-net/panel/api/controller"
 )
 
 func PublisherPlace(r *gin.RouterGroup) {
-	// TODO
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "ad-fetcher.html", nil)
+	})
 }
 
 func PublisherReport(r *gin.RouterGroup) {
@@ -18,18 +19,100 @@ func PublisherReport(r *gin.RouterGroup) {
 }
 
 func PublisherFinance(r *gin.RouterGroup) {
-	// TODO
-}
-
-
-func Publisher(r *gin.RouterGroup) {
+	ctrl := controller.NewPublisherController()
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "publisher_name.html", nil)
+		c.HTML(http.StatusOK, "my_finance.html", nil)
 	})
-	pubPlace := r.Group("/place")
-	pubReport := r.Group("/report")
-	pubFinance := r.Group("/finance")
+
+	r.GET("/balance", func(c *gin.Context) {
+		uid, err := strconv.Atoi(c.Param("publisher_id"))
+		id := uint(uid)
+		if err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		publisher, err := ctrl.GetPublisher(id)
+		if err != nil {
+			c.AbortWithError(http.StatusNotFound, err)
+			return
+		}
+
+		c.HTML(http.StatusOK, "balance.html", gin.H{"Name": publisher.Name, "Balance": publisher.Balance})
+	})
+
+	r.GET("/payment", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "payment.html", nil)
+	})
+
+	r.POST("/add-credit", func(c *gin.Context) {
+		uid, err := strconv.Atoi(c.Param("publisher_id"))
+		id := uint(uid)
+		if err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+		amount, err := strconv.Atoi(c.PostForm("amount"))
+		if err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		publisher, err := ctrl.GetPublisher(id)
+		if err != nil {
+			c.AbortWithError(http.StatusNotFound, err)
+			return
+		}
+		publisher.Balance += amount
+		ctrl.DB.Save(publisher)
+
+		c.Redirect(http.StatusFound, "/publisher/"+strconv.Itoa(int(publisher.ID))+"/finance/balance")
+	})
+
+}
+
+func Publisher(r *gin.RouterGroup) {
+	ctrl := controller.NewPublisherController()
+
+	r.GET("/", func(c *gin.Context) {
+		publishers, err := ctrl.GetPublishers()
+		if err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+		c.HTML(http.StatusOK, "publisher.html", gin.H{"Publishers": publishers})
+	})
+
+	r.POST("/submit-name", func(c *gin.Context) {
+		name := c.PostForm("name")
+		publisher, err := ctrl.NewPublisher(name)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		c.Redirect(http.StatusFound, "/publisher/"+strconv.Itoa(int(publisher.ID)))
+	})
+
+	r.GET("/:publisher_id", func(c *gin.Context) {
+		uid, err := strconv.Atoi(c.Param("publisher_id"))
+		id := uint(uid)
+		if err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		publisher, err := ctrl.GetPublisher(id)
+		if err != nil {
+			c.AbortWithError(http.StatusNotFound, err)
+			return
+		}
+		c.HTML(http.StatusOK, "pub_dashboard.html", gin.H{"Name": publisher.Name, "ID": publisher.ID})
+	})
+
+	pubPlace := r.Group("/:publisher_id/add-script")
+	pubReport := r.Group("/:publisher_id/reports")
+	pubFinance := r.Group("/:publisher_id/finance")
 
 	PublisherPlace(pubPlace)
 	PublisherReport(pubReport)
--- Panel/main.go
+++ Panel/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-//	"log"
+	"log"
 
 	"github.com/sarrkar/chan-ta-net/panel/api"
-//	"github.com/sarrkar/chan-ta-net/panel/database"
+	"github.com/sarrkar/chan-ta-net/panel/database"
 )
 
 func main() {
-/*
+
 	err := database.InitDb()
 	defer database.CloseDb()
 	if err != nil {
 		log.Fatalln(err)
 	}
-*/
+
 	api.InitServer()
 }
